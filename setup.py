2c7d663243891b1187ca56ebbf4864a3ee878cd0
gompert <joshua.gompert@gmail.com>
2026-01-02 20:03:17 -1000

    added pyproject.toml and updated setup.py for pypi distribution

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="qrpypass",
-    version="0.1.0",
+    version="0.1.1",
     description="Headless QR decoder + TOTP authenticator Flask mini-service",
     author="Josh Gompert",
     author_email="",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=[],
+    install_requires=[
+        "opencv-python>=4.8.0",
+        "cryptography>=41.0.0",
+        "Flask>=3.0.0",
+        "qrcode[pil]>=7.4.2",
+        "Pillow>=10.0.0",
+    ],
     python_requires=">=3.9",
 )
